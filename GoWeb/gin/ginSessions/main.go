3670ef7551f6f1029142262b6000495249c64d25
LeiLei <imleilei123@gmail.com>
2022-02-20 11:36:32 +0800

    2.20 修改sessions的案例

--- GoWeb/gin/ginSessions/main.go
+++ GoWeb/gin/ginSessions/main.go
@@ -9,16 +9,19 @@ import (
 func main() {
 	r := gin.Default()
 	store := cookie.NewStore([]byte("secret"))
+	//注入中间件
 	r.Use(sessions.Sessions("mysession", store))
 
 	r.GET("/hello", func(c *gin.Context) {
-		session := sessions.Default(c)
+		session := sessions.Default(c) //实例化sessions
 
-		if session.Get("hello") != "world" {
+		if session.Get("hello") != "world" { //获得sessions并判断
+			//不等于则设置
 			session.Set("hello", "world")
+			//保存
 			session.Save()
 		}
-
+		//输出一个key为hello,value为sessions
 		c.JSON(200, gin.H{"hello": session.Get("hello")})
 	})
 	r.Run(":8000")
