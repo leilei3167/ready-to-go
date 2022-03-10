8f5effaaacae5fee4387ddfe58e4181886f99f01
lei_lei <imleilei123@gmail.com>
2022-03-10 20:40:39 +0800

    增加翻转字符串的思路

--- Algorithm/reversSlice/main.go
+++ Algorithm/reversSlice/main.go
@@ -6,8 +6,7 @@ func main() {
 	before := []int{1, 2, 3, 4, 5}
 	after := revers(before)
 	fmt.Println("reverse1", after)
-	fmt.Println("reverse2", revers2(before))
-
+	fmt.Println("test:", revers2("abcdef"))
 }
 
 func revers(s []int) []int {
@@ -19,10 +18,12 @@ func revers(s []int) []int {
 	return s
 }
 
-func revers2(s []int) []int {
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		s[i], s[j] = s[j], s[i]
+//翻转字符串,转化为byte切片后,通过交换下标的形式
+func revers2(s string) string {
+	s1 := []byte(s)
+	for i, j := 0, len(s1)-1; i < j; i, j = i+1, j-1 {
+		s1[i], s1[j] = s1[j], s1[i]
 
 	}
-	return s
+	return string(s1)
 }
