a0fdf73646b019d37f1969eec7bf16944860fd10
lei <imleilei123@gmail.com>
2022-04-21 09:48:51 +0800

    benchmark

---
+++ UltimateGo/7.Testing/benchmark/bench_test.go
@@ -0,0 +1,34 @@
+package benchmark_test
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+)
+
+//benchmark最重要的是for循环
+//go test -bench . -benchtime=3s -benchmem
+//执行所有测试
+func BenchmarkSprintf(b *testing.B) {
+	num := 10
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		fmt.Sprintf("%d", num)
+	}
+}
+
+func BenchmarkFormat(b *testing.B) {
+	num := int64(10)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		strconv.FormatInt(num, 10)
+	}
+}
+
+func BenchmarkItoa(b *testing.B) {
+	num := 10
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		strconv.Itoa(num)
+	}
+}
