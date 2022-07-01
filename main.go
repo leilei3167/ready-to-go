0bb4ae8b807fe5cdaa0eeae3861bb5affb81213a
lei <imleilei123@gmail.com>
2022-07-01 17:56:13 +0800

    feat: 测试

--- main.go
+++ main.go
@@ -1,117 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"log"
-	"strconv"
-	"strings"
 )
 
 func main() {
-	a := make([]int, 0)
-	s := strings.Split("1,2,3,45,6,1-47", ",")
-	fmt.Printf("s以逗号分割后s的值:%#v\n", s)
-	for _, v := range s {
-		//判断是否是范围表达
-		if strings.Contains(v, "-") {
-			ports, err := Scope(v)
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Printf("Scope后的:%#v", ports)
-			a = append(a, ports...)
-		} else {
-			//不是范围表达转换为单个数字
-			x, err := strconv.Atoi(v)
-			if err != nil {
-				log.Fatal("输入不合法,端口间以逗号分隔,范围以开始-结束处表示", err)
-			}
-			a = append(a, x)
-		}
+	a := "123456789"
+	b := make([]byte, 3)
 
-	}
-	a = delrep(a)
-	fmt.Printf("a去重后的值:%#v\n", a)
+	n := copy(b, []byte(a))
 
-	x := ""
-	x = strings.TrimSpace(x)
-	fmt.Printf("X的值为:%#v\n", x)
-	
+	b = b[:n]
 
-	for i := 0; i < 5; i++ {
-		from := (8 / 3) * i //直接取整,舍弃小数
-		log.Println(from)
+	fmt.Printf("a的长度:%d 真实:%#v,len:%d,cap:%d\n", len(a), b, len(b), cap(b))
 
+	p := Person{
+		Name: "大家扫",
+		Age:  18,
+		Job: JobInfo{
+			Company: "腾讯",
+			//	Salary:  10000,
+			Work: map[string]interface{}{"职责": "研发", "工作地点": "北京"},
+		},
 	}
 
-	depart(123, 4)
+	byt, _ := json.Marshal(p)
+
+	var p1 interface{}
+	json.Unmarshal(byt, &p1)
+
+	fmt.Printf("%#v\n", p1)
 
 }
 
-//处理范围端口
-func Scope(v string) ([]int, error) {
-	//以-来切分开始和结束端口
-	tri := strings.SplitN(v, "-", 2) //切分成开始和结束2个部分
-	log.Printf("SplitN:%#v", tri)
-	//将开始和结束解析为int,出错说明输入有误
-	start, err := strconv.Atoi(tri[0])
-	if err != nil {
-		return nil, err
-	}
-	end, err := strconv.Atoi(tri[1])
-	if err != nil {
-		return nil, err
-	}
-	//以start和end来构建ports切片
-	n := end - start + 1
-	if n < 1 {
-		return nil, fmt.Errorf("结束端口%v小于开始端口%v", start, end)
-	}
-	log.Printf("start:%#v,end:%#v,n:%v", start, end, n)
-	ports := make([]int, n)
-	for i := 0; i < n; i++ {
-		ports[i] = start
-		start++
-	}
-	return ports, nil
+type Person struct {
+	Name string `json:"name,omitempty"`
+	Age  int    `json:"age,omitempty"`
 
+	Job JobInfo `json:"job,omitempty"`
 }
-
-//字符串去重
-func delrep(v []int) []int {
-	tmp := make(map[int]struct{})
-	result := make([]int, 0)
-
-	for _, v := range v {
-		_, ok := tmp[v]
-		if ok {
-			continue
-		} else {
-			tmp[v] = struct{}{}
-			result = append(result, v)
-		}
-
-	}
-	return result
-}
-
-func depart(len, g int) {
-	fmt.Println("----------------------------------------------------------------------------------")
-	var result []string
-	for i := 0; i < g; i++ {
-		from := (len / g) * i
-		var to int
-		if i < g-1 {
-			to = (len/g)*(i+1) - 1
-		} else {
-			to = len
-		}
-		result = append(result, strconv.Itoa(from)+"-"+strconv.Itoa(to))
-	}
-
-	for _, r := range result {
-		fmt.Println(r)
-	}
-	fmt.Println("----------------------------------------------------------------------------------")
-
+type JobInfo struct {
+	Company string                 `json:"company,omitempty"`
+	Salary  int                    `json:"salary,omitempty"`
+	Work    map[string]interface{} `json:"work,omitempty"`
 }
