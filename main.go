21eb69114be2fe3d2bd6d3bc449305a1233c92e9
lei <imleilei123@gmail.com>
2022-07-07 13:31:29 +0800

    docs: LookupAddr

--- main.go
+++ main.go
@@ -2,12 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
+	"os"
 )
 
 func main() {
-	nums := make([]int, 5)
-	for i := 0; i < len(nums); i++ {
-		nums[i] = i * i
+
+	ip, err := net.LookupHost("www.2szy.cn")
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
 	}
-	fmt.Println(nums)
+	fmt.Printf("%+v\n", ip)
+
+	ips, err := net.LookupIP("www.qq.com")
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	fmt.Printf("%v\n", ips)
+
+	addr, err := net.LookupAddr("121.14.77.221")
+
+	fmt.Printf("%#v\n", addr)
+
 }
