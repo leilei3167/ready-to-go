456932d2e40b1326784d9a9d69899cffc6bf95a7
lei <imleilei123@gmail.com>
2022-07-11 16:54:57 +0800

    feat: Bytes.Count

--- hello/main.go
+++ hello/main.go
@@ -1,13 +1,43 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"time"
+	"io"
+	"log"
+	"os"
 )
 
 func main() {
-	for i := 0; i < 10; i++ {
-		fmt.Println("hello world!")
-		time.Sleep(time.Second)
+
+	file, err := os.Open("/home/lei/code/ready-to-go/MDtest.md")
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	count := lineCounter(file)
+	fmt.Println(count)
+
+}
+
+//count the number of line in a file
+func lineCounter(src io.Reader) int {
+	buf := make([]byte, 32*1024)
+
+	count := 0
+	sep := []byte{'\n'}
+
+	for {
+		c, err := src.Read(buf)
+		count += bytes.Count(buf[:c], sep)
+
+		switch {
+		case err == io.EOF:
+			return count
+
+		case err != nil:
+			return -1
+		}
+	}
+
 }
